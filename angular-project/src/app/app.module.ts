1d8d044124c4d67ae5373173fd3657d8ae00ae14
asm-dev <salamanca.m@outlook.com>
2022-11-28 15:23:07 +0100

    separate initial global html into angular components

--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { ProjectSpecsComponent } from './project-specs/project-specs.component';
+import { UpcomingEventsComponent } from './upcoming-events/upcoming-events.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { StandardsComponent } from './standards/standards.component';
+import { PartnersComponent } from './partners/partners.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HeroSectionComponent,
+    ProjectSpecsComponent,
+    UpcomingEventsComponent,
+    ProjectsComponent,
+    StandardsComponent,
+    PartnersComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ angular-project/src/app/project-specs/project-specs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-specs',
+  templateUrl: './project-specs.component.html',
+  styleUrls: ['./project-specs.component.css']
+})
+export class ProjectSpecsComponent {
+
+}
