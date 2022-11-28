a092717c7fbd4772f230199c82fed05d3c5cd661
Blanca <blancabcbt@gmail.com>
2022-11-28 15:10:23 +0100

    html and styles

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './COMPONENTES/header/header.component';
+import { MainComponent } from './COMPONENTES/main/main.component';
+import { HeroComponent } from './COMPONENTES/main/hero/hero.component';
+import { ArticleComponent } from './COMPONENTES/main/article/article.component';
+import { EventComponent } from './COMPONENTES/main/event/event.component';
+import { ProjectComponent } from './COMPONENTES/main/project/project.component';
+import { PartnersComponent } from './COMPONENTES/main/partners/partners.component';
+import { FooterComponent } from './COMPONENTES/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    HeroComponent,
+    ArticleComponent,
+    EventComponent,
+    ProjectComponent,
+    PartnersComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
